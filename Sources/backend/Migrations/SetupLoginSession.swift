070ecb896e10567ad03c3edfcaa83f5c06ceb7aa
makabaka1880 <makabaka1880@outlook.com>
2025-08-07 16:45:31 +0800

    feat: added tables and migration for login session table

---
+++ Sources/backend/Migrations/SetupLoginSession.swift
@@ -0,0 +1,29 @@
+// Created by Sean L. on Aug. 7.
+// Last Updated by Sean L. on Aug. 7.
+// 
+// Tea Charlie - Backend
+// Sources/backend/Migrations/SetupLoginSession.swift
+// 
+// Makabaka1880, 2025. All rights reserved.
+
+import Vapor
+import Fluent
+
+struct CreateLoginSession: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        return database.schema("login_sessions")
+            .id()
+            .field("user_id", .uuid, .required)
+            .field("session_key", .uuid, .required)
+            .field("created_at", .datetime, .required)
+            .field("user_agent", .string)
+            .field("ip_address", .string)
+            .unique(on: "session_key")
+            .foreignKey("user_id", references: "users", "id", onDelete: .cascade)
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        return database.schema("login_sessions").delete()
+    }
+}
